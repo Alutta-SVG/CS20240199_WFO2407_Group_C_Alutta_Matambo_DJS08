a92dae98ddc6cae02d71b99154a1ab9d2ca55d48
Alutta <Aluttamatambo5@gmail.com>
2024-11-17 09:31:12 +0200

    Added imports and exoport default to Hostlayout

---
+++ StudentNo_Classcode_Group_Name-Surname_DJS08-main/Components/HostLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import {NavLink, Outlet} from "react-router-dom"
+
+export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+}
