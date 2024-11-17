0f4eee43916ea722a3c197e2830c6404e958ae0b
Alutta <Aluttamatambo5@gmail.com>
2024-11-17 10:09:06 +0200

    Created Home.jsx and added imports and exports and home function

---
+++ StudentNo_Classcode_Group_Name-Surname_DJS08-main/src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to #VanLife</h1>
+      <p>Discover the freedom of van living.</p>
+    </div>
+  );
+}
